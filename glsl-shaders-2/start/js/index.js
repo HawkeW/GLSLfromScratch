a6a3978a6efca14b853588436f46b3fb02a37230
Onceonce <hughew@foxmail.com>
2022-11-13 23:37:46 +0800

    learn 1 & 2

--- glsl-shaders-2/start/js/index.js
+++ glsl-shaders-2/start/js/index.js
@@ -1,77 +1,79 @@
 const vshader = `
 void main() {	
-  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( position * 0.5, 1 );
 }
-`
+`;
 const fshader = `
 uniform vec3 u_color;
 uniform vec2 u_mouse;
 uniform vec2 u_resolution;
 uniform float u_time;
-
 void main (void)
 {
-  vec3 color = vec3(u_mouse.x/u_resolution.x, 0.0, u_mouse.y/u_resolution.y);
-  //color = vec3((sin(u_time)+1.0)/2.0, 0.0, (cos(u_time)+1.0)/2.0);
-  gl_FragColor = vec4(color, 1.0); 
+  // vec2 v = u_mouse / u_resolution;
+  // vec3 color = vec3(v.x, 0, v.y);
+  vec3 color = vec3((sin(u_time) + 1.0) / 2.0, 0.0, (cos(u_time) + 1.0) / 2.0);
+  gl_FragColor = vec4(color, 1.0).brga; 
 }
-`
-
-
-
-
-
+`;
 
 const scene = new THREE.Scene();
-const camera = new THREE.OrthographicCamera( -1, 1, 1, -1, 0.1, 10 );
+const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 const clock = new THREE.Clock();
 
-const geometry = new THREE.PlaneGeometry( 2, 2 );
+const geometry = new THREE.PlaneGeometry(2, 2);
+
 const uniforms = {
-  u_color: { value: new THREE.Color(0xff0000) },
-  u_time: { value: 0.0 },
-  u_mouse: { value:{ x:0.0, y:0.0 }},
-  u_resolution: { value:{ x:0, y:0 }}
+  u_color: {value: new THREE.Color(0x0000ff)},
+  u_resolution: {value: {x: 0, y: 0}},
+  u_mouse: {value: {x: 0, y: 0}},
+  u_time: {value: 0},
+};
+
+if ("ontouchstart" in window) {
+  window.addEventListener("touchmove", move);
+} else {
+  window.addEventListener("resize", onWindowResize);
+  window.addEventListener("mousemove", move);
 }
 
-const material = new THREE.ShaderMaterial( {
-  uniforms: uniforms,
-  vertexShader: vshader,
-  fragmentShader: fshader
-} );
+function move(evt) {
+  if (evt.touches) {
+    uniforms.u_mouse.value.x = evt.touches[0].clientX;
+    uniforms.u_mouse.value.y = evt.touches[0].clientY;
+  } else {
+    uniforms.u_mouse.value.x = evt.clientX;
+    uniforms.u_mouse.value.y = evt.clientY;
+  }
+}
 
-const plane = new THREE.Mesh( geometry, material );
-scene.add( plane );
+const material = new THREE.ShaderMaterial({
+  uniforms,
+  vertexShader: vshader,
+  fragmentShader: fshader,
+});
+
+const plane = new THREE.Mesh(geometry, material);
+scene.add(plane);
 
 camera.position.z = 1;
 
 onWindowResize();
-if ('ontouchstart' in window){
-  document.addEventListener('touchmove', move);
-}else{
-  window.addEventListener( 'resize', onWindowResize, false );
-  document.addEventListener('mousemove', move);
-}
-
-function move(evt){
-  uniforms.u_mouse.value.x = (evt.touches) ? evt.touches[0].clientX : evt.clientX;
-  uniforms.u_mouse.value.y = (evt.touches) ? evt.touches[0].clientY : evt.clientY;
-}
 
 animate();
 
-function onWindowResize( event ) {
-  const aspectRatio = window.innerWidth/window.innerHeight;
+function onWindowResize(event) {
+  const aspectRatio = window.innerWidth / window.innerHeight;
   let width, height;
-  if (aspectRatio>=1){
+  if (aspectRatio >= 1) {
     width = 1;
-    height = (window.innerHeight/window.innerWidth) * width;
-  }else{
+    height = (window.innerHeight / window.innerWidth) * width;
+  } else {
     width = aspectRatio;
     height = 1;
   }
@@ -80,13 +82,16 @@ function onWindowResize( event ) {
   camera.top = height;
   camera.bottom = -height;
   camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  uniforms.u_resolution.value.x = window.innerWidth;
-  uniforms.u_resolution.value.y = window.innerHeight;
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  if (uniforms.u_resolution !== undefined) {
+    uniforms.u_resolution.value.x = window.innerWidth;
+    uniforms.u_resolution.value.y = window.innerHeight;
+  }
 }
 
 function animate() {
-  requestAnimationFrame( animate );
-  uniforms.u_time.value += clock.getDelta();
-  renderer.render( scene, camera );
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  uniforms.u_time.value = clock.getElapsedTime();
 }
